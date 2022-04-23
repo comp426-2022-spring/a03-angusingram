b38f8f02e3f7c30ba508653c71fe8d980c244b69
angusingram <angus214@ad.unc.edu>
2022-04-23 16:02:33 -0400

    fixed quotes missing

--- server.js
+++ server.js
@@ -85,16 +85,16 @@ function countFlips(array) {
         tails++;
       }
     }
-    return "{\"heads\":" + heads + ",\"tails\":" + tails + "}";
+    return '{"heads":' + heads + ',"tails":' + tails + '}';
   }
 
 function flipACoin(call) {
     if (call == "heads" || call == "tails") {
       let x = coinFlip();
       if (x == call) {
-        return "{ call: '" + call + "', flip: '" + x + "', result: 'win' }"
+        return '{"call":"' + call + '","flip":"' + x + '","result":"win"}'
       } else {
-        return "{ call: '" + call + "', flip: " + x + ", result: 'lose' }"
+        return '{"call":"' + call + '","flip":"' + x + '","result":"lose"}'
       }
     }
     
